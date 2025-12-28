3dd21f83ad26b27ab323cb857c70b7a61ca8b915
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2025-12-28 14:56:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-    server: {
-    host: true, // Allows network access
-    port: 5173, // Optional: Specify a port
+
+  server: {
+    host: true,
+    port: 5173,
+
+    allowedHosts: [
+      'rawarfrontend.onrender.com',
+    ],
   },
 });
